71939a0ba52a09ed6ea1763e49f69f6a14886669
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2023-03-13 17:31:56 +0700

    api table score each league

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
 /**
  * 
@@ -13,7 +16,7 @@ const axios = require('axios');
     7 : J League
  * @returns table score of this league
  */
-async function scarpe (league) {
+const scarpeTable = async (league) => {
     const url = 'https://www.soccersuck.com/';
     const containerTag = 'div#container';
 
@@ -27,7 +30,37 @@ async function scarpe (league) {
         const item = $(containerTag);
         const table = $(item).find(tableTag).text();
 
-        return table;
+        // return table;
+        const lines = table.split('\n').map(line => line.trim());
+        const result = [];
+        let temp = '';
+        for (let i = 0; i < lines.length; i++) {
+            temp += lines[i] + ' ';
+
+            if ((i + 1) % 11 === 0) {
+                result.push(temp.trim());
+                temp = '';
+            }
+        }
+
+
+        const newTable = result.map((item) => {
+            const arr = item.split(' '); // split the string into an array
+            return {
+                Number: arr[0],
+                Team: arr.slice(1, -8).join(' '), // join the team name with spaces [start with name and end with the first element of match]
+                Match: arr[arr.length - 8],
+                Win: arr[arr.length - 7],
+                Draw: arr[arr.length - 6],
+                Lost: arr[arr.length - 5],
+                Scored: arr[arr.length - 4],
+                Conceded: arr[arr.length - 3],
+                Difference: arr[arr.length - 2],
+                Point: arr[arr.length - 1]
+            };
+        });
+
+        return newTable;
     }
     catch {
         console.log('error');
@@ -35,37 +68,29 @@ async function scarpe (league) {
 }
 
 
-scarpe(1).then((data) => {
-    // split the line by \n and mapping every with trim to clear the space that we don't want it
-    // concatenate and trim it every 11 line before push the each team to array
-    const lines = data.split('\n').map(line => line.trim());
-    const result = [];
-    let temp = '';
-    for (let i = 0; i < lines.length; i++) {
-        temp += lines[i] + ' ';
+app.get('/', (req, res) =>{
+    return res.json({test: 'api in running'});
+});
 
-        if ((i + 1) % 11 === 0) {
-            result.push(temp.trim());
-            temp = '';
+
+app.get('/api/table/:league', async (req, res) => {
+    const league = req.params.league;
+    const checkLeague = Number(league);
+    if (checkLeague > 0 && checkLeague < 8) {
+        const table = await scarpeTable(league);
+
+        const data = {
+            message: 'ok',
+            table: table
         }
+
+        return res.status(200).json(data);
     }
 
+    return res.status(404).json({message: 'invalid league number'});
+});
 
-    const newData = result.map((item) => {
-        const arr = item.split(' '); // split the string into an array
-        return {
-            Number: arr[0],
-            Team: arr.slice(1, -8).join(' '), // join the team name with spaces [start with name and end with the first element of match]
-            Match: arr[arr.length - 8],
-            Win: arr[arr.length - 7],
-            Draw: arr[arr.length - 6],
-            Lost: arr[arr.length - 5],
-            Scored: arr[arr.length - 4],
-            Conceded: arr[arr.length - 3],
-            Difference: arr[arr.length - 2],
-            Point: arr[arr.length - 1]
-        };
-    });
-      
-    console.log(newData);
+
+app.listen(8080, () => {
+    console.log('server running at http://localhost:8080');
 });
